1a1c98e103916818fdaad9e9c63cda301f448056
Sohel Momin <momin8805887375@gmail.com>
2025-07-30 21:21:12 +0530

    yout tube clone project pushed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './Hpme.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Feed from '../../Components/Feed/Feed'
+
+const Home = ({sidebar}) => {
+
+
+  const [category,setCategory] = useState(0)
+
+
+  
+  return (
+   <>
+   <Sidebar sidebar={sidebar} category={category} setCategory={setCategory}/>
+   <div className={`container ${sidebar?"":'large-container'}`}>
+    <Feed category={category}/>
+   </div>
+
+   </>
+  )
+}
+
+export default Home
